d8cb1711abb406d95c442bc9356cae97ec3fc20b
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-12-11 11:38:09 +0530

    Pass 1 program code

--- Pass 1/pass1.c
+++ Pass 1/pass1.c
@@ -4,80 +4,80 @@
 #include <stdbool.h>
 
 void main(){
-	FILE *inter, *symtab, *optab, *src, *len;
+	FILE *source, *optab, *intermediate, *symtab, *length;
 
-	optab=fopen("optab.txt","r");
-	symtab=fopen("symtab.txt","w+");
-	inter=fopen("intermediate.txt","w");
-	src=fopen("source.txt","r");
-	len=fopen("length.txt","w");
+	source = fopen("source.txt", "r");
+	optab = fopen("optab.txt", "r");
+	intermediate = fopen("intermediate.txt", "w");
+	symtab = fopen("symtab.txt", "w+");
+	length = fopen("length.txt", "w");
 
-	char label[20], opcode[20], operand[20], sym_label[20], optab_code[20];
-	int locctr, starting_address, prgrm_length;
-	bool sym_found, op_found;
+	char label[20], opcode[20], operand[20], symtab_data[20], optab_data[20];
+	int locctr, starting_address, program_length;
+	bool symbol_found, opcode_found;
 
-	fscanf(src, "%s%s%s", label, opcode, operand);
+	fscanf(source, "%s%s%s", label, opcode, operand);
 	if(strcmp(opcode, "START") == 0) {
 		starting_address = atoi(operand);
 		locctr = starting_address;
-		fprintf(inter, "\t\t%s\t%s\t%s\n", label, opcode, operand);
+		fprintf(intermediate, "\t\t%s\t\t%s\t%s\n", label, opcode, operand);
 	} else {
 		locctr = 0;
 	}
 
-	fscanf(src, "%s%s%s", label, opcode, operand);
+	fscanf(source, "%s%s%s", label, opcode, operand);
 	while(strcmp(opcode, "END") != 0) {
 		if(strcmp(label, "**") != 0) {
-			sym_found = false;
+			symbol_found = false;
 			rewind(symtab);
 			while(!feof(symtab)) {
-				fscanf(symtab, "%s", sym_label);
-				if(strcmp(sym_label, label) == 0) {
+				fscanf(symtab, "%s", symtab_data);
+				if(strcmp(symtab_data, label) == 0) {
 					printf("Error: Label found in SYMTAB");
-					sym_found = true;
+					symbol_found = true;
 				}
 			}
-			if(sym_found == false) {
+			if(symbol_found == false) {
 				fprintf(symtab, "%s\t%d\n", label, locctr);
 			}
 		}
-		op_found = false;
+		opcode_found = false;
 		rewind(optab);
 		while(!feof(optab)) {
-			fscanf(optab, "%s", optab_code);
-			if(strcmp(optab_code, opcode) == 0) {
-				op_found = true;
+			fscanf(optab, "%s", optab_data);
+			if(strcmp(optab_data, opcode) == 0) {
+				opcode_found = true;
 			}
 		}
-		fprintf(inter, "%d\t", locctr);
-		if(op_found == true) {
+		fprintf(intermediate, "%d\t", locctr);
+		if(opcode_found == true) {
 			locctr += 3;
 		}
 		else if(strcmp(opcode, "WORD") == 0) {
 			locctr += 3;
 		}
-		else if(strcmp(opcode, "RESW") == 0){
+		else if(strcmp(opcode, "RESW") == 0) {
 			locctr += 3*atoi(operand);
 		}
-		else if(strcmp(opcode, "RESB") == 0){
+		else if(strcmp(opcode, "RESB") == 0) {
 			locctr += atoi(operand);
 		}
-		else if(strcmp(opcode, "BYTE") == 0){
+		else if(strcmp(opcode, "BYTE") == 0) {
 			locctr = locctr+strlen(operand)-3;
 		}
 		else {
 			printf("Error: Invalid operation code");
 		}
-		fprintf(inter, "%s\t%s\t%s\n", label, opcode, operand);
-		fscanf(src, "%s%s%s", label, opcode, operand);
+		fprintf(intermediate, "%s\t%s\t%s\n", label, opcode, operand);
+		fscanf(source, "%s%s%s", label, opcode, operand);
 	}
-	fprintf(inter, "%d\t%s\t%s\t%s\n", locctr, label, opcode, operand);
-	prgrm_length = locctr-starting_address;
-	printf("\nProgram length: %d\n\n", prgrm_length);
-	fprintf(len, "%d",prgrm_length);
-	fclose(len);
-	fclose(inter);
+	fprintf(intermediate, "%d\t%s\t\t%s\t\t%s\n", locctr, label, opcode, operand);
+	program_length = locctr-starting_address;
+	printf("\nProgram length: %d\n\n", program_length);
+	fprintf(length, "%d", program_length);
+	fclose(length);
+	fclose(intermediate);
 	fclose(symtab);
 	fclose(optab);
-	fclose(src);
+	fclose(source);
 }
